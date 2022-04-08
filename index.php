b1f2acf53ef9c91d0425398f09fcc894a22c1a35
Melichou <silvafernandesmelissa@gmail.com>
2022-04-08 18:14:38 +0200

    created interface for trashes + worked on plastic trashes

--- index.php
+++ index.php
@@ -5,16 +5,12 @@ require_once 'Classes/Services/MetalRecyclor.php';
 require_once 'Classes/Services/PaperRecyclor.php';
 require_once 'Classes/Services/PlasticRecyclor.php';
 require_once 'Classes/Services/Compost.php';
-require_once 'Classes/Trashes/PVC.php';
+require_once 'Classes/Trashes/PC.php';
+require_once 'Classes/Trashes/Paper.php';
+require_once 'Classes/Trashes/Glass.php';
 require_once 'Classes/JSONDecoder.php';
 require_once 'Classes/JSONCutter.php';
 
-$plasticR = new PlasticRecyclor(0);
-$pvc = new PVC('test',20,5);
+      
 
-$plastics = $plasticR->addPlastic($pvc);
-print_r($plastics);
-
-$test = $plasticR->getTotalCapacity();
-echo $test;
 ?>
