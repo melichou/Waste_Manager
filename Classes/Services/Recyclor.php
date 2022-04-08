e82db0e7a626fc1d3462a397643d0d6607ff366d
Melichou <silvafernandesmelissa@gmail.com>
2022-04-08 22:47:32 +0200

    implemented an interface to create service object and added it to services

--- Classes/Services/Recyclor.php
+++ Classes/Services/Recyclor.php
@@ -2,24 +2,10 @@
 require_once 'Classes/Service.php';
 abstract class Recyclor extends Service{
     //Attributes
-    //protected string $name;
     protected int $capacity;
 
     //Getters & setters
-    /* public function getName() : string{
-        return $this->name;
-    }
-    public function setName(string $name) : self{
-        $this->name = $name;
-        return $this;
-    } */
 
-    /* public function getCapacity() : int{
-        return $this->capacity;
-    }
-    public function setCapacity(int $capacity) : self{
-        $this->capacity = $capacity;
-        return $this;
-    } */
+    
 }
 ?>
