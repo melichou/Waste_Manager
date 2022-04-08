bdd9c62a3381db6e4093890b8b0e073aa0502ec0
Melichou <silvafernandesmelissa@gmail.com>
2022-04-09 00:03:37 +0200

    implemented interface on plastic recyclor class

--- Classes/Services/PlasticRecyclor.php
+++ Classes/Services/PlasticRecyclor.php
@@ -4,9 +4,11 @@ require_once 'Classes/Trashes/Plastic.php';
 require_once 'Classes/JSONDecoder.php';
 require_once 'Classes/JSONCutter.php';
 require_once 'Interfaces/GetCapaServInterface.php';
-class PlasticRecyclor extends Recyclor implements GetCapaServInterface{
+require_once 'Interfaces/CreateServInterface.php';
+class PlasticRecyclor extends Recyclor implements GetCapaServInterface, CreateServInterface{
     //Attributes
     private array $types;
+    private array $plast;
 
     //Constructor 
     public function __construct(int $capacity){
@@ -20,6 +22,13 @@ class PlasticRecyclor extends Recyclor implements GetCapaServInterface{
         }
         $this->types[] = $plastic;
         return $this->types;
+    }
+    public function getPlast() : array{
+        return $this->plast;
+    }
+    public function setPlast(array $plast) : self{
+        $this->plast = $plast;
+        return $this;
     }
     //Interfaces functions
     public function getTotalCapacity() : int
@@ -41,6 +50,33 @@ class PlasticRecyclor extends Recyclor implements GetCapaServInterface{
         return $capa;
               
     }
+    public function createServ()
+    {
+        //Variables
+        $type = 'recyclagePlastique'; // because it's the MetalRecyclor class
+        $mykey = "plastiques";
+        $capa = 0;
+        $plast = array();
+        $obj = null;
+        $cutter = new JSONCutter();
+        $serv = $cutter->getCut(GetCapaServInterface::json,'services');
     
+
+        foreach ($serv as $i=>$value) {
+            $array = $serv[$i];
+            if($array["type"] == $type){
+                $types = $value["plastiques"];
+                foreach ($types as $j=>$v){
+                    $plast[] = $v;
+                }
+                $capa += $value["capacite"];
+                $current = new PlasticRecyclor($capa);
+                $current->setPlast($plast);
+                $obj[] = $current;
+                unset($plast);
+            }
+        }
+        return $obj;
+    }
 }
 ?>
